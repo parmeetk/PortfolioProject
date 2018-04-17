71d6d6a2ebf954e384dbe9288d440502a628c4b6
Parmeet Koonar <parmeetk.1998@gmail.com>
2018-04-16 18:45:24 -0700

    Had to make a new repository because the other one had an error.

--- TopDownShooter/Assets/Scripts/PlayerController.cs
+++ TopDownShooter/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,39 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
     }
+
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
 }
